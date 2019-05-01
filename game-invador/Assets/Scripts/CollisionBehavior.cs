7327bf76bbf0ea17e76ffa9d3f20e26c64606ee6
yu-mao <yjybxx2@yahoo.com>
2019-05-01 21:27:28 +0200

    Invador, adding audio effect.

--- game-invador/Assets/Scripts/CollisionBehavior.cs
+++ game-invador/Assets/Scripts/CollisionBehavior.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class CollisionBehavior : MonoBehaviour
 {
+    public AudioSource Audio;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Projectile")
         {
             Destroy(collision.gameObject);
             GameManager.instance.OnDestroyEnemy();
+
+            if (collision.gameObject.tag == "Enemy")
+            {
+                //var audio = collision.collider.GetComponent<AudioSource>();
+                //audio.Play();
+                Audio.Play();
+                Debug.Log(Audio.name);
+            }
+
+            //var audioLala = gameObject.GetComponent<AudioSource>();
+            //audioLala.Play();
+            //Debug.Log("Audio");
+            //Audio.Play(0);
+            //transform.GetComponent<SoundEffect>().PlayAudio();
         }
     }
 }
